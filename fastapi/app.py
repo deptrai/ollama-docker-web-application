e3e8fb6acd770afa76bc405a7873f17a6365f7e2
Nguyen Phuong Anh Tu <tuan8165@gmail.com>
2024-11-18 11:44:04 +0700

    DEV_BY_AnhTuDev

--- fastapi/app.py
+++ fastapi/app.py
@@ -13,7 +13,7 @@ def ask(prompt :str):
     res = requests.post('http://ollama:11434/api/generate', json={
         "prompt": prompt,
         "stream" : False,
-        "model" : "qwen2.5-coder:0.5b"
+        "model" : "llama3"
     })
 
     return Response(content=res.text, media_type="application/json")
--- ollama/pull-qwen.sh
+++ ollama/pull-qwen.sh
@@ -6,7 +6,7 @@ pid=$!
 sleep 5
 
 
-echo "Pulling qwen2 model"
+echo "Pulling qwen2.5-coder model"
 ollama pull qwen2.5-coder:0.5b
 
 
