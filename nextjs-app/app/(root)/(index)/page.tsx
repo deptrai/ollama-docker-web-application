df5240e4ca7fea47547ed1cea56b65ec414e928b
KilopFD <tuan8165@gmail.com>
2024-11-20 23:57:44 +0700

    DEPLOYMENT DEV BY 8SYNCDEV - Anh Tu Dev

--- nextjs-app/app/(root)/(index)/page.tsx
+++
@@ -1,7 +0,0 @@
-import dynamic from 'next/dynamic';
-
-import ChatInterface from './render-page';
-
-export default function Home() {
-  return <ChatInterface />;
-}
--- nextjs-app/lib/actions/const.ts
+++
@@ -1,1 +0,0 @@
-export const URL_BACKEND = "http://127.0.0.1:8000"
